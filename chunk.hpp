0af46024adb67df499be3451496cd3e2f2790aed
merwanel <>
2024-08-07 14:53:29 +0200

    first

---
+++ chunk.hpp
@@ -0,0 +1,9 @@
+// chunk refer to sequences of bytecode
+
+#include <cstdint>
+#include <vector>
+
+typedef enum {
+  OP_RETURN,
+} OpCode; // operation codes
+
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <algorithm>
+
+
+
+int main(int argc, char const *argv[]) {
+  std::vector<uint8_t> c;
+  return 0;
+}
