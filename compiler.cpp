99197b602fb3dd126bdc715ee81c07a8485aa301
merwanel <>
2024-08-14 19:44:17 +0200

    scanner test

--- compiler.cpp
+++ compiler.cpp
@@ -1,6 +1,17 @@
 #include <string>
 #include "./scanner.cpp"
 #include <iostream>
+#include <iomanip>
+
+std::string get_token_type_str(TokenType type) {
+    #define GET(p) case(p): return #p;
+    switch (type) {
+        GET(TOKEN_LEFT_PAREN);GET( TOKEN_RIGHT_PAREN); GET(TOKEN_LEFT_BRACE); GET( TOKEN_RIGHT_BRACE); GET(TOKEN_COMMA); GET( TOKEN_DOT); GET( TOKEN_MINUS); GET( TOKEN_PLUS); GET(TOKEN_SEMICOLON); GET( TOKEN_SLASH); GET( TOKEN_STAR); GET(TOKEN_BANG); GET( TOKEN_BANG_EQUAL); GET( TOKEN_EQUAL); GET( TOKEN_EQUAL_EQUAL); GET( TOKEN_GREATER); GET( TOKEN_GREATER_EQUAL); GET( TOKEN_LESS); GET( TOKEN_LESS_EQUAL); GET( TOKEN_IDENTIFIER); GET( TOKEN_STRING); GET( TOKEN_NUMBER); GET( TOKEN_AND); GET( TOKEN_CLASS); GET( TOKEN_ELSE); GET( TOKEN_FALSE); GET(TOKEN_FOR); GET( TOKEN_FUN); GET( TOKEN_IF); GET( TOKEN_NIL); GET( TOKEN_OR); GET( TOKEN_PRINT); GET( TOKEN_RETURN); GET( TOKEN_SUPER); GET( TOKEN_THIS); GET(TOKEN_TRUE); GET( TOKEN_VAR); GET( TOKEN_WHILE); GET( TOKEN_ERROR); GET( TOKEN_EOF);
+    }
+    #undef GET
+
+    return "";
+}
 
 void compile(const std::string source) {
     Scanner scanner(source);
@@ -13,8 +24,9 @@ void compile(const std::string source) {
         } else {
             printf("   | ");
         }
-        printf("%2d ", token.type);
-        std::cout << "'" << scanner.source.substr(token.length, token.start)  <<  "'" <<  std::endl ;
+        std::string type = get_token_type_str(token.type);
+        std::cout << std::setw(25) << std::left << type ;
+        std::cout << "'" << scanner.source.substr(token.start, token.length)  <<  "'\t" << token.start << " "<< token.length <<  std::endl ;
         if (token.type == TOKEN_EOF) break;
     }
 }
--- hello_test.cc
+++ hello_test.cc
@@ -4,81 +4,121 @@
 
 #include "./chunk.cpp"
 #include "./debug.cpp"
-#include "vm.cpp"
+#include "./vm.cpp"
 
-// 1 + 2 * 3 - 4 / -5    
-TEST(Disassembler, op) {
+std::string TokensToStr(std::string& source , std::vector<Token>& tokens) {
     testing::internal::CaptureStdout();
-
-    Chunk chunk;
-    
-    writeConstant(chunk, 1, 1);
-    writeConstant(chunk, 2, 1);
-    writeChunk(chunk, OP_ADD, 1);
-    writeConstant(chunk, 3, 2);
-    writeChunk(chunk, OP_MULTIPLY, 2);
-    writeConstant(chunk, 4, 3);
-    writeChunk(chunk, OP_SUBTRACT, 3);
-    writeConstant(chunk, 5, 3);
-    writeChunk(chunk, OP_DIVIDE, 4);
-    
-    writeChunk(chunk, OP_RETURN, 1234);
-
-    disassembleChunk(chunk, "test chunk");
-    
-    std::string output = testing::internal::GetCapturedStdout();
-    std::string expected = "== test chunk ==\n"
-                        "0000    1 OP_CONSTANT         0 '1'\n"
-                        "0002    | OP_CONSTANT         1 '2'\n"
-                        "0004    | OP_ADD\n"
-                        "0005    | OP_CONSTANT         2 '3'\n"
-                        "0007    2 OP_MULTIPLY\n"
-                        "0008    | OP_CONSTANT         3 '4'\n"
-                        "0010    | OP_SUBTRACT\n"
-                        "0011    | OP_CONSTANT         4 '5'\n"
-                        "0013    4 OP_DIVIDE\n"
-                        "0014 1234 OP_RETURN\n";
-    EXPECT_EQ(output, expected);
-}
-TEST(Disassembler, op_constant_long) {
-    testing::internal::CaptureStdout();
-
-    Chunk chunk;
-    for(int i = 0 ; i < 263 ; i++) {
-        writeConstant(chunk, i, i);
+    std::string str = ""; 
+    int line = -1;
+    for (Token token : tokens) {
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        std::string type = get_token_type_str(token.type);
+        std::cout << std::setw(25) << std::left << type ;
+        std::cout << "'" << source.substr(token.start, token.length)  <<  "'" << std::setw(10) << std::right << token.start << " "<< token.length <<  std::endl ;
+        if (token.type == TOKEN_EOF) break;
     }
-    disassembleChunk(chunk, "test chunk");
-    
-    std::string output = testing::internal::GetCapturedStdout();
-    int start_last_line = output.size()-1 ;
-    while (output[--start_last_line] != '\n') ;
-    std::string last_line = output.substr(start_last_line+1);
+    return testing::internal::GetCapturedStdout();
 
-    std::string expected = "0536    | OP_CONSTANT_LONG  262 '262'\n";
-    EXPECT_EQ(last_line, expected);
 }
-
 // 1 + 2 * 3 - 4 / -5    
-TEST(VM, binary_op) {
-    testing::internal::CaptureStdout();
+// TEST(Disassembler, op) {
+//     testing::internal::CaptureStdout();
 
-    Chunk chunk;
+//     Chunk chunk;
     
-    writeConstant(chunk, 1, 1);
-    writeConstant(chunk, 2, 1);
-    writeChunk(chunk, OP_ADD, 1);
-    writeConstant(chunk, 3, 2);
-    writeChunk(chunk, OP_MULTIPLY, 2);
-    writeConstant(chunk, 4, 3);
-    writeChunk(chunk, OP_SUBTRACT, 3);
-    writeConstant(chunk, 5, 3);
-    writeChunk(chunk, OP_DIVIDE, 4);
+//     writeConstant(chunk, 1, 1);
+//     writeConstant(chunk, 2, 1);
+//     writeChunk(chunk, OP_ADD, 1);
+//     writeConstant(chunk, 3, 2);
+//     writeChunk(chunk, OP_MULTIPLY, 2);
+//     writeConstant(chunk, 4, 3);
+//     writeChunk(chunk, OP_SUBTRACT, 3);
+//     writeConstant(chunk, 5, 3);
+//     writeChunk(chunk, OP_DIVIDE, 4);
     
-    writeChunk(chunk, OP_RETURN, 1234);
+//     writeChunk(chunk, OP_RETURN, 1234);
 
-    VM vm = initVM(chunk);
-    interpret(vm);
-    std::string output = testing::internal::GetCapturedStdout();
-    std::string expected = "1\n";
-    EXPECT_EQ(output, expected);
+//     disassembleChunk(chunk, "test chunk");
+    
+//     std::string output = testing::internal::GetCapturedStdout();
+//     std::string expected = "== test chunk ==\n"
+//                         "0000    1 OP_CONSTANT         0 '1'\n"
+//                         "0002    | OP_CONSTANT         1 '2'\n"
+//                         "0004    | OP_ADD\n"
+//                         "0005    | OP_CONSTANT         2 '3'\n"
+//                         "0007    2 OP_MULTIPLY\n"
+//                         "0008    | OP_CONSTANT         3 '4'\n"
+//                         "0010    | OP_SUBTRACT\n"
+//                         "0011    | OP_CONSTANT         4 '5'\n"
+//                         "0013    4 OP_DIVIDE\n"
+//                         "0014 1234 OP_RETURN\n";
+//     EXPECT_EQ(output, expected);
+// }
+// TEST(Disassembler, op_constant_long) {
+//     testing::internal::CaptureStdout();
+
+//     Chunk chunk;
+//     for(int i = 0 ; i < 263 ; i++) {
+//         writeConstant(chunk, i, i);
+//     }
+//     disassembleChunk(chunk, "test chunk");
+    
+//     std::string output = testing::internal::GetCapturedStdout();
+//     int start_last_line = output.size()-1 ;
+//     while (output[--start_last_line] != '\n') ;
+//     std::string last_line = output.substr(start_last_line+1);
+
+//     std::string expected = "0536    | OP_CONSTANT_LONG  262 '262'\n";
+//     EXPECT_EQ(last_line, expected);
+// }
+TEST(Scanner, print) {
+    std::string source = "print 1 + 2;";
+    Scanner scanner(source);
+    std::vector<Token> tokens;
+    for (;;) {
+        tokens.push_back(scanner.scanToken());
+        if (tokens.back().type == TOKEN_EOF) break;
+    }
+    std::string output = TokensToStr(source, tokens);
+    
+    std::vector<Token> expectedTokens{
+        {TOKEN_IDENTIFIER, 0, 5, 1},
+        {TOKEN_NUMBER, 6, 1, 1},
+        {TOKEN_PLUS, 8, 1, 1},
+        {TOKEN_NUMBER, 10, 1, 1},
+        {TOKEN_SEMICOLON, 11, 1, 1},
+        {TOKEN_EOF, 12, 0, 1},
+    };
+    std::string expected_str = TokensToStr(source, expectedTokens);
+    EXPECT_EQ(output, expected_str);
 }
+
+// // 1 + 2 * 3 - 4 / -5    
+// TEST(VM, binary_op) {
+//     testing::internal::CaptureStdout();
+
+//     Chunk chunk;
+    
+//     writeConstant(chunk, 1, 1);
+//     writeConstant(chunk, 2, 1);
+//     writeChunk(chunk, OP_ADD, 1);
+//     writeConstant(chunk, 3, 2);
+//     writeChunk(chunk, OP_MULTIPLY, 2);
+//     writeConstant(chunk, 4, 3);
+//     writeChunk(chunk, OP_SUBTRACT, 3);
+//     writeConstant(chunk, 5, 3);
+//     writeChunk(chunk, OP_DIVIDE, 4);
+    
+//     writeChunk(chunk, OP_RETURN, 1234);
+
+//     VM vm = initVM(chunk);
+//     interpret(vm);
+//     std::string output = testing::internal::GetCapturedStdout();
+//     std::string expected = "1\n";
+//     EXPECT_EQ(output, expected);
+// }
